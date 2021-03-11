7b5b45a6d123c34bb51dfc6a53b4dc03e88dad32
Ian Ramos <ianramossantos@hotmail.com>
2021-03-11 20:45:25 -0300

    feat: update todo

--- src/index.js
+++ src/index.js
@@ -65,7 +65,18 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params
+  const { title, deadline } = request.body
+  const { user } = request
+
+  const todo = user.todos.find( todo => todo.id === id)
+
+  if (!todo) return response.status(404).json({ error: 'Todo not found!' })
+
+  todo.title = title
+  todo.deadline = new Date(deadline)
+
+  return response.status(200).json(todo)
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
