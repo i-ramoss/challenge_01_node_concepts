01b9a318f942ea5826e4bd668a54bb19c4ef9922
Ian Ramos <ianramossantos@hotmail.com>
2021-03-11 21:06:01 -0300

    feat: update todo as finished

--- src/index.js
+++ src/index.js
@@ -80,7 +80,16 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params
+  const { user } = request
+
+  const todo = user.todos.find( todo => todo.id === id)
+
+  if (!todo) return response.status(404).json({ error: 'Todo not found!' })
+
+  todo.done = true
+
+  return response.status(200).json(todo)
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
